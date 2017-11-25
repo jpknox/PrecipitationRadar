9453f8cd3e3ac38a29becad7c79fb60c564f0509
J P Knox <joaoknox@hotmail.com>
2017-11-25 16:01:57 +0000

    Patchwork canvas drawing but it is in the wrong place.

--- Official_overlay_example/MetOffice.js
+++ Official_overlay_example/MetOffice.js
@@ -1,5 +1,5 @@
 function MetOffice() {
-	var matrixVersion = 0;
+	var matrixVersion = 1;
 	var dimension = Math.pow(2, matrixVersion);
 	var tileEdgeLength = 256;
 //	var baseUrl = "http://datapoint.metoffice.gov.uk/public/data/inspire/view/wmts?REQUEST=gettile&LAYER=RADAR_UK_Composite_Highres&FORMAT=image/png&TILEMATRIXSET=EPSG:29903&TILEMATRIX=EPSG:29903:" + matrixVersion;
@@ -26,18 +26,16 @@ function MetOffice() {
 				metOfficeDataUrl.tileColValue = x;
 				mapTile = new Image();
 				mapTile.src = metOfficeDataUrl.completeUrl();
-				mapTile.onload = wrapDrawImage(context, mapTile, x, y);
+				mapTile.crossOrigin = "anonymous";
+				mapTile.onload = wrapDrawImage(mapTile, context, x, y, canvas);
 				console.log("Drawing a tile originating at x:" + (x % dimension) * tileEdgeLength + " y:" + (y % dimension) * tileEdgeLength + "\t with URL:" + metOfficeDataUrl.completeUrl());
 				mapTiles[tileIndex++] = mapTile;
 			}
 		}
-
-		//canvas.style.width = window.screen.width;
-		console.log("Canvas data Url: " + canvas.toDataURL("image/png"))
-		return canvas.toDataURL("image/png");
+		return canvas;
 	}
 
-	var wrapDrawImage = function(context, mapTile, x, y) {
+	var wrapDrawImage = function(mapTile, context, x, y, canvas) {
 		return function() {
 					context.drawImage(mapTile, (x % dimension) * tileEdgeLength, (y % dimension) * tileEdgeLength);
 				};
@@ -63,7 +61,7 @@ function metOfficeUrl() {
 	this.tileMatrixValue = '0';
 	this.tileRowValue = '0';
 	this.tileColValue = '0';
-	this.dimRunValue = '2017-11-24T13:30:00Z';
+	this.dimRunValue = '2017-11-25T15:30:00Z';
 	this.keyValue = '5b2b0c85-7f41-47a6-9b53-87f842d08ca5';
 	
 	this.completeUrl = function() {
